5826abeae84bb6d54e879bd859f2099af53084a7
JT Turner <j.tturner14@yahoo.com>
2019-11-12 23:32:10 -0500

    started html/responsive

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// FUNCTION REQUIREMENTS/////////////////////
+// save events for each hour of day
+// 9:00 - 5:00
+    // each time slot displays
+        // time
+        // field for input 
+        // save button 
+            // stores time and user input in localStorage
+
+// DISPLAY REQUIREMENTS///////////////////////
+// near top shows current day
+// colors show rather timeslot is in the past, present, or future (changes depending on the hour)
+    // use https://momentjs.com/ to work with date and time
+
+
